8c4cc5534c31c49f4052ffc55130b986563f612c
Borovyk-Mykola <114910601+Borovyk-Mykola@users.noreply.github.com>
2023-06-02 16:55:05 +0300

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+
+export class App extends React.Component {
+state ={
+  value: '',
+}
+
+onSubmitClick = data => {
+  this.setState({value: data.value})
+}
+
+render() {
+    return (
+      <div className="App"
+        style={{
+          height: '100vh',
+          display: 'block',
+          fontSize: 40,
+          color: '#010101',
+        }}>
+        <Searchbar onSubmit={this.onSubmitClick}/>
+        <ImageGallery value={this.state.value}/>
+      </div>
+    )
+  }
 };
